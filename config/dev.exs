d1bba52b05a91fd98d5e7d77ff287df2ef641c26
Rauulll <nyaokeraul003@gmail.com>
2024-08-15 17:38:01 +0300

    update runtime.exs

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :discuss, Discuss.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "discuss_test",
+  database: "discuss_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
