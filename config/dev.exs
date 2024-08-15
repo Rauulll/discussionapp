1e6aa4a5350c3f9ffcc4fdf82d1b985b261e22b1
Rauulll <nyaokeraul003@gmail.com>
2024-08-15 16:37:40 +0300

    update dev

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :discuss, Discuss.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "discuss_dev",
+  database: "discuss_test",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
